b2ed9601dd5f1befebd4124b0e67b71ce32d6843
harpreet-singh-147 <h_singh_007@hotmail.co.uk>
2024-02-18 23:05:14 +0000

    moved modals to other pages to create a better homepage. Also deleted unwanted code and files

--- public/script/index.js
+++ public/script/index.js
@@ -1,11 +1,10 @@
 import { handleAuthUi } from "./handleAuthUi.js";
-import { button, body } from "./selectors.js";
+import { body } from "./selectors.js";
 import { handleRegisterSubmit } from "./registerAccount.js";
 import { handleLogin } from "./handleLogin.js";
 import { handleContactMe } from "./handleContactMe.js";
 import { handleLogoutClick } from "./handleLogout.js";
 import { handleTipCalculator } from "./tipCalculator.js";
-import { alert } from "./alert.js";
 import { modal } from "./modal.js";
 
 document.addEventListener("DOMContentLoaded", () => {
@@ -25,10 +24,4 @@ document.addEventListener("DOMContentLoaded", () => {
 	handleContactMe();
 	handleLogoutClick();
 	handleTipCalculator();
-
-	if (button) {
-		button.addEventListener("click", () => {
-			alert();
-		});
-	}
 });
